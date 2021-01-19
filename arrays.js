e2aed988347921baf2bf10491891339ba5eb0e5b
Lisa Swart <arnoudlisa@gmail.com>
2021-01-19 23:21:06 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -23,3 +23,12 @@ function destructivelyAddElementToEndOfArray(array, element) {
 function accessElementInArray(array, index) {
   return array[index]
 }
+
+function destructivelyRemoveElementFromBeginningOfArray(array) {
+  array.shift()
+  return array
+}
+
+function removeElementFromBeginningOfArray(array) {
+  var chocBars = array.slice(1)
+}
